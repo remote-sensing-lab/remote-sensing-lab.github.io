e7d8cf6ecf3429a76e1a917eba6c70ccd6a56f85
Qiusheng Wu <giswqs@gmail.com>
2018-03-28 22:36:23 -0400

    update contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,7 +11,7 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
-	    var gotcha = $("input#_gotcha").val();
+	          var gotcha = $("input#_gotcha").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
